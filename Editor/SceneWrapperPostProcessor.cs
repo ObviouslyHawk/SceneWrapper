cc8498414dc22864b083779551694955dd334f0c
Aleksei <kuzhelalexej@mail.ru>
2023-08-15 10:45:05 +0300

    Changed SceneWrapper update logic.
    UPD: SceneWrapper now updates when SceneAsset changed it's name or if Scene's build index is changed.

---
+++ Editor/SceneWrapperPostProcessor.cs
@@ -0,0 +1,96 @@
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GexagonVR.SceneWrapper
+{
+    [InitializeOnLoad]
+    public class SceneWrapperPostProcessor : AssetPostprocessor
+    {
+        static SceneWrapperPostProcessor()
+        {
+            EditorBuildSettings.sceneListChanged += SceneListChanged;
+        }
+
+        private static void SceneListChanged()
+        {
+            string[] sceneWrapperGUIDs = AssetDatabase.FindAssets($"t:{typeof(SceneWrapper).Name}");
+            SceneWrapper[] sceneWrapperInstances = new SceneWrapper[sceneWrapperGUIDs.Length];
+
+            for (int i = 0; i < sceneWrapperGUIDs.Length; i++)
+            {
+                sceneWrapperInstances[i] = AssetDatabase.LoadAssetAtPath<SceneWrapper>(AssetDatabase.GUIDToAssetPath(sceneWrapperGUIDs[i]));
+            }
+
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                SceneAsset sceneAsset = AssetDatabase.LoadAssetAtPath(EditorBuildSettings.scenes[i].path, typeof(SceneAsset)) as SceneAsset;
+                SceneWrapper sceneWrapperInstance = sceneWrapperInstances.First(x => x.SceneGUID == EditorBuildSettings.scenes[i].guid.ToString());
+
+                if (sceneWrapperInstance == null)
+                {
+                    continue;
+                }
+
+                if (SceneUtility.GetBuildIndexByScenePath(EditorBuildSettings.scenes[i].path) == sceneWrapperInstance.SceneBuildIndex)
+                {
+                    continue;
+                }
+
+                ((ISceneWrapperSceneSetter)sceneWrapperInstance).SetSceneData(sceneAsset.name, SceneUtility.GetBuildIndexByScenePath(EditorBuildSettings.scenes[i].path));
+            }
+        }
+
+        public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            for (int i = 0; i < movedAssets.Length; i++)
+            {
+                SceneAsset sceneAsset = AssetDatabase.LoadAssetAtPath(movedAssets[i], typeof(SceneAsset)) as SceneAsset;
+
+                if (!sceneAsset)
+                {
+                    return;
+                }
+
+                SetSceneWrapperData(sceneAsset);
+            }
+        }
+
+        private static void SetSceneWrapperData(SceneAsset sceneAsset)
+        {
+            bool isSceneNameUpdated = false;
+
+            foreach (string sceneWrapperGUID in AssetDatabase.FindAssets($"t:{typeof(SceneWrapper).Name}"))
+            {
+                SceneWrapper sceneWrapperInstance = AssetDatabase.LoadAssetAtPath<SceneWrapper>(AssetDatabase.GUIDToAssetPath(sceneWrapperGUID));
+
+                if (sceneWrapperInstance == null)
+                {
+                    continue;
+                }
+
+                AssetDatabase.TryGetGUIDAndLocalFileIdentifier(sceneAsset, out string changedSceneGUID, out long _);
+                if (sceneWrapperInstance.SceneGUID == changedSceneGUID)
+                {
+                    ((ISceneWrapperSceneSetter)sceneWrapperInstance).SetSceneData(sceneAsset.name, sceneWrapperInstance.SceneBuildIndex);
+                    AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(sceneWrapperInstance), sceneAsset.name);
+                    isSceneNameUpdated = true;
+                    break;
+                }
+            }
+
+            if (isSceneNameUpdated)
+            {
+                EditorApplication.update += DelayedRefreshOnSceneNameChanged;
+            }
+        }
+
+        private static void DelayedRefreshOnSceneNameChanged()
+        {
+            AssetDatabase.Refresh();
+            EditorApplication.update -= DelayedRefreshOnSceneNameChanged;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SceneWrapper
 Click on any SceneAsset and click "Create Scene Wrapper" to create scene wrapper for it.
 Use this asset as you would SceneManager, but without using any string literals as it already contains all necessary data.
+SceneWrapper automatically updates when scene is renamed or when scene build index is changed.
